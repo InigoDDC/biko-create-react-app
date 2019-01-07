16609d11dff649b245567478c4014a0447114856
Ruben Bernardez <ruben.bernardez@biko2.com>
2019-01-08 00:29:16 +0100

    remove storybook config and demo component

--- packages/react-scripts/template-typescript/src/views/_components/Demo/Demo.stories.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-import { ThemeProvider } from '../../theme'
-import { Demo } from './Demo'
-
-storiesOf('Demo', module)
-  .addDecorator(story => <ThemeProvider>{story()}</ThemeProvider>)
-  .add('default', () => <Demo>Hello world!!</Demo>)
--- packages/react-scripts/template-typescript/src/views/_components/Demo/Demo.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from '../../theme'
-
-export const Demo = styled.div`
-  border: 1px solid gray;
-  padding: 10px;
-  ${p => p.theme.font.base()};
-`
