f777179ca6055345b4cd133eb5a36d3c0105ee2c
Daniel <daniel.eguzkiza@gmail.com>
2019-04-10 21:38:25 +0200

    corregida la configuración del nombre que se da en desarrollo a los bundles

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -186,7 +186,7 @@ module.exports = function(webpackEnv) {
       // In development, it does not produce real files.
       filename: isEnvProduction
         ? 'static/js/[name].[chunkhash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+        : isEnvDevelopment && 'static/js/[name].js', //BIKO:modified from 'static/js/bundle.js'
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
         ? 'static/js/[name].[chunkhash:8].chunk.js'
